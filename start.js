153c396fc43861db3618099b76d06add1b51c3a7
stephclleung <35399643+stephclleung@users.noreply.github.com>
2019-09-01 01:34:05 -0400

    Loading env

--- start.js
+++ start.js
@@ -1,6 +1,8 @@
 const app = require('./app.js');
+require('dotenv').config();
 const PORT = process.env.PORT //|| 5002;
 console.log(`App at port ${PORT}`)
+
 app.listen(PORT, () => {
     console.log(`Server up at ${PORT}`);
 })
